c6a2970b4669a2f550c7f04b3df0c81f284ac3bd
Jcorrieri <jacomocorrieri@gmail.com>
2025-02-08 21:43:58 -0500

    added java directory and started assignment 1

---
+++ java/src/Main.java
@@ -0,0 +1,82 @@
+import java.util.Arrays;
+
+class Buffer {
+    public static final int EMPTY = 0;
+    private static final int FULL = 1;
+    private final int[] buffer;
+    private int capacity;
+
+    public Buffer(int size) {
+        buffer = new int[size];
+        this.capacity = 0;
+    }
+
+//    public int size() {return buffer.length;}
+
+    public synchronized void produce(int index) throws InterruptedException {
+        while (capacity == buffer.length) { // buffer is full
+            System.out.println(Arrays.toString(buffer) + " - Buffer is full, producer waiting...");
+            wait();
+        }
+        Thread.sleep(500); // simulate writing
+        if (buffer[index] == EMPTY) {
+            System.out.println(Arrays.toString(buffer) + " - Writing FULL (1) to element " + index);
+            buffer[index] = FULL;
+            capacity++;
+        } else {
+            System.out.println(Arrays.toString(buffer) + " - Element " + index + " is FULL, skipping operation");
+        }
+        notify();
+    }
+
+    public synchronized void consume(int index) throws InterruptedException {
+        while (capacity == 0) {
+            System.out.println(Arrays.toString(buffer) + " - Buffer is empty, consumer waiting...");
+            wait();
+        }
+        Thread.sleep(500); // simulate reading
+        if (buffer[index] == FULL) {
+            System.out.println(Arrays.toString(buffer) + " - Consuming element " + index);
+            buffer[index] = EMPTY;
+            capacity--;
+        } else {
+            System.out.println(Arrays.toString(buffer) + " - Element " + index + " is EMPTY, skipping operation");
+        }
+        notify();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Building buffer of size: " + args[0]);
+        int size = Integer.parseInt(args[0]);
+        final Buffer buffer = new Buffer(size);
+//        System.out.println(Arrays.toString(buffer));
+
+        Thread producer = new Thread(() -> {
+           int index = 0;
+           while (true) {
+               try {
+                   buffer.produce(index % size);
+                   index++;
+               } catch (InterruptedException e) {
+                   throw new RuntimeException(e);
+               }
+           }
+        });
+
+        Thread consumer = new Thread(() -> {
+            int index = 0;
+            while (true) {
+                try {
+                    buffer.consume(index % size);
+                    index++;
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        producer.start();
+        consumer.start();
+    }
+}
